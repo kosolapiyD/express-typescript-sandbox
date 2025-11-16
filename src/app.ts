05bca72aa0b91ba6d35b34d36e8302e07d107057
Medved Dima <kosolapiyd@gmail.com>
2025-11-16 19:02:44 +0200

    Refactor app routes: separate home route into its own module

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import routes from './routes';
-import usersRoutes from './routes/users.route';
+import homeRouter from './routes/home.route';
+import usersRouter from './routes/users.route';
 import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/', routes);
-app.use('/api/users', usersRoutes);
+app.use('/', homeRouter);
+app.use('/api/users', usersRouter);
 
 // Error handler
 app.use(errorHandler);
